15cb43642bb7db793f504acb9efc930c882562e0
mlpierce22 <mlpierce22@gmail.com>
2023-11-17 16:41:34 -0800

    Fix ' escape

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -21,7 +21,7 @@ export const UnderConstruction = () => {
                 />
                 <Heading as="h2" size="lg" bgGradient='linear(to-r, red.500, yellow.500)'
                     bgClip='text' mt={4}>Page Under Construction</Heading>
-                <Text mt={2}>Thanks for checking out my site! I'm still working on it but it should be up soon!</Text>
+                <Text mt={2}>{"Thanks for checking out my site! I'm still working on it but it should be up soon!"}</Text>
             </Box>
         </Center>
     );
