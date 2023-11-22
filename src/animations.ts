d602a36b559d05f1ed4896beb416d9bd17e470b7
mlpierce22 <mlpierce22@gmail.com>
2023-11-22 10:37:52 -0800

    Add animations ts file

---
+++ src/animations.ts
@@ -0,0 +1,13 @@
+/** A list of useful animations for fun */
+import { keyframes } from "@emotion/react";
+export const hammer = keyframes`
+0% { transform: rotate(-45deg); }
+50% { transform: rotate(45deg); }
+            100% { transform: rotate(-45deg); }
+`;
+
+export const wave = keyframes`
+0% { transform: rotate(-45deg); }
+50% { transform: rotate(90deg); }
+            100% { transform: rotate(-45deg); }
+`;
