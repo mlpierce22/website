503ccca2eae2dbe7152caa75a2be4e1595bd9281
mlpierce22 <mlpierce22@gmail.com>
2023-11-28 23:00:30 -0800

    remove custom color script

--- src/components/CustomColorScript.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import dynamic from 'next/dynamic';
-
-export const CustomColorScript = dynamic(
-    () => import('@chakra-ui/react').then((mod) => mod.ColorModeScript),
-    { ssr: false }
-);
