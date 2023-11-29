e97ea5d9c742e7ec2b3ef378b8618bbfa180204f
mlpierce22 <mlpierce22@gmail.com>
2023-11-28 18:55:55 -0800

    Add fonts to tailwind and fix missing buttons, add color theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,10 +1,34 @@
 import {
-    type ThemeOverride, extendTheme
+    type ThemeOverride, extendTheme, withDefaultColorScheme, theme as baseTheme
 } from '@chakra-ui/react'
+
+/** Can import 100->700 (ibm) or 100-900 (poppins) each with italic variant. Import as necessary */
+import '@fontsource/ibm-plex-serif/100.css';
+import '@fontsource/ibm-plex-serif/400.css';
+import '@fontsource/ibm-plex-serif/400-italic.css';
+import '@fontsource/ibm-plex-serif/700.css';
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/400-italic.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/900.css';
 
 const config: ThemeOverride = {
     initialColorMode: 'dark',
     useSystemColorMode: true,
 }
 
-export default extendTheme({ config });
+const fonts = {
+    body: '"IBM Plex Serif", sans-serif',
+    heading: 'Poppins, sans-serif',
+}
+const colors = {
+    brand: baseTheme.colors.blue,
+}
+
+export default extendTheme(
+    {
+        config, fonts, colors
+    },
+    withDefaultColorScheme({ colorScheme: 'brand' }));
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,14 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
+    fontFamily: {
+      display: ['"IBM Plex Serif"', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
