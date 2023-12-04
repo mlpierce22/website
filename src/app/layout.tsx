168f626e328e22c15aeb27dbf86b6a3e3ff251ec
mlpierce22 <mlpierce22@gmail.com>
2023-12-04 01:43:55 -0800

    Make analytics id public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.ANALYTICS_ID}');
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
           `}
         </Script>
         { }
