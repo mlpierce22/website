17e3ff46bd6db02fc0bac306dfcdee374f771823
mlpierce22 <mlpierce22@gmail.com>
2023-12-04 01:44:06 -0800

    custom prose for about me page

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,2 +1,9 @@
-import Layout from '@/app/prose-layout'
-export default Layout
+import { Box } from "@chakra-ui/react";
+
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <Box as="div" p={10} className="prose md:prose-xl prose-stone dark:prose-invert overflow-scroll">
+            {children}
+        </Box>
+    )
+}
