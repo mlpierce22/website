1685b0564dc3632939915b3a68d76f392e2960b2
mlpierce22 <mlpierce22@gmail.com>
2023-12-04 01:33:11 -0800

    Revert "Hide back on about page"
    This reverts commit b6eac766781cd241bbe9fbd8e7854e8523e62526.

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,10 +1,2 @@
 import Layout from '@/app/prose-layout'
-import React from 'react'
-
-export default function AboutLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <Layout hideBack={true}>
-            {children}
-        </Layout>
-    )
-}
+export default Layout
--- src/app/prose-layout.tsx
+++ src/app/prose-layout.tsx
@@ -3,13 +3,11 @@ import { Box, Button, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { IoMdArrowBack } from "react-icons/io";
 
-export default function MdxLayout({ children, hideBack }: { children: React.ReactNode, hideBack?: boolean }) {
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
     return (
         <VStack w={"100%"} h={"100%"}>
-            {!!!hideBack && (
-                <Button leftIcon={<IoMdArrowBack />} onClick={() => router.back()} alignSelf={"start"} mt={10}>Back</Button>
-            )}
+            <Button leftIcon={<IoMdArrowBack />} onClick={() => router.back()} alignSelf={"start"} mt={10}>Back</Button>
             <Box as="div" p={10} className="prose md:prose-xl prose-stone dark:prose-invert overflow-scroll">
                 {children}
             </Box>
