dba2a1bf885b971e143d28c12a7afd1eb39b5493
mlpierce22 <mlpierce22@gmail.com>
2023-12-04 16:30:28 -0800

    fix funky mobile width

--- src/app/prose-layout.tsx
+++ src/app/prose-layout.tsx
@@ -6,11 +6,8 @@ import { IoMdArrowBack } from "react-icons/io";
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
     return (
-        <VStack w={"100%"} h={"100%"}>
-            <Button leftIcon={<IoMdArrowBack />} onClick={() => router.back()} alignSelf={"start"} mt={10}>Back</Button>
-            <Box as="div" p={10} className="prose md:prose-xl prose-stone dark:prose-invert overflow-scroll">
-                {children}
-            </Box>
-        </VStack>
+        <Box as="div" p={10} className="prose md:prose-xl prose-stone dark:prose-invert overflow-scroll w-screen" >
+            {children}
+        </Box >
     )
 }
